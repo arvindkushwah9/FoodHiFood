125fa8c07114301300a02681d901b5ccb8b83936
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-01-07 14:00:08 +0530

    First commit

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,34 @@
+class CheckoutController < ApplicationController
+  protect_from_forgery :except => [:create]
+  def show
+    if logged_in?
+      @current_order.ordered_items = session[:order]["items"]
+    else
+      redirect_to login_path
+    end
+  end
+
+  def create
+    if order_params[:status].downcase === "completed" || order_params[:status].downcase === "pending"
+      @current_order.update_order(session[:order], order_params)
+      if @current_order.save_order(@current_user)
+        session[:order] = {}
+        session[:cart] = {}
+        flash[:success] = "Your order has been successfully placed."
+        # OrderWorker.perform_async(@current_user.id, @current_order.to_json)
+        UserMailer.order_email(@current_user, @current_order.to_json).deliver
+
+        redirect_to root_path
+      else
+        flash[:error] = "An error occured while saving your order. Please try again."
+        redirect_to cart_checkout_path
+      end
+    end
+  end
+  private
+
+  def order_params
+    { :invoice => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id]}
+  end
+
+end
